4e655a25370a35712c570376c9271cd7c23bd2bd
inmacc96 <inmacaballerocarrero@gmail.com>
2022-04-19 18:21:58 +0200

    The Prisma database cannot be queried on the client side.

--- meals-order-app/pages/index.js
+++ meals-order-app/pages/index.js
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
 import { PrismaClient } from "@prisma/client";
 
 export default function Home({ categories }) {
+  useEffect(() => {
+    const consultDB = async () => {
+      const prisma = new PrismaClient();
+
+      const categories = await prisma.category.findMany();
+      console.log(categories);
+    };
+    consultDB();
+  }, []);
   console.log(categories);
   return <h1>Next JS</h1>;
 }
