418bdb144c14ba50a6cda40564424616c04135e5
inmacc96 <inmacaballerocarrero@gmail.com>
2022-04-19 18:01:50 +0200

    Query database with  getServerSideProps.

--- meals-order-app/pages/index.js
+++ meals-order-app/pages/index.js
@@ -1,3 +1,18 @@
-export default function Home() {
+import { PrismaClient } from "@prisma/client";
+
+export default function Home({ categories }) {
+  console.log(categories);
   return <h1>Next JS</h1>;
 }
+
+export const getServerSideProps = async () => {
+  const prisma = new PrismaClient();
+
+  const categories = await prisma.category.findMany();
+
+  return {
+    props: {
+      categories,
+    },
+  };
+};
