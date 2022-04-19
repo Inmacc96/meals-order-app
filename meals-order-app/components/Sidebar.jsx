cbff92fdab7fdba07f7ba29df1c9e35f148343c3
inmacc96 <inmacaballerocarrero@gmail.com>
2022-04-19 19:25:32 +0200

    Get the categories from the API and display them in the sidebar.

--- meals-order-app/components/Sidebar.jsx
+++ meals-order-app/components/Sidebar.jsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
+import useMealsOrder from "../hooks/useMealsOrder";
+import Category from "./Category";
 
 const Sidebar = () => {
+  const { categories } = useMealsOrder();
   return (
     <>
       <Image
@@ -9,7 +12,12 @@ const Sidebar = () => {
         src="/assets/img/logo.svg"
         alt="logo image"
       />
-      
+
+      <nav className="mt-10">
+        {categories.map((category) => (
+          <Category key={category.id} category={category} />
+        ))}
+      </nav>
     </>
   );
 };
--- meals-order-app/context/MealsOrderProvider.jsx
+++ meals-order-app/context/MealsOrderProvider.jsx
@@ -1,10 +1,22 @@
 import { useState, useEffect, createContext } from "react";
+import axios from "axios";
 
 const MealsOrderContext = createContext();
 
 const MealsOrderProvider = ({ children }) => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const getCategories = async () => {
+      const { data } = await axios("/api/categories");
+      setCategories(data);
+    };
+
+    getCategories();
+  }, []);
+
   return (
-    <MealsOrderContext.Provider value={{}}>
+    <MealsOrderContext.Provider value={{categories}}>
       {children}
     </MealsOrderContext.Provider>
   );
