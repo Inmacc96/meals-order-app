404fc5a4074a41402df3018fa4fe1c497489c344
inmacc96 <inmacaballerocarrero@gmail.com>
2022-04-20 17:35:10 +0200

    Add functionality to adjust quantities.

--- meals-order-app/components/ProductModal.jsx
+++ meals-order-app/components/ProductModal.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import Image from "next/image";
 import useMealsOrder from "../hooks/useMealsOrder";
 import { formatMoney } from "../helpers";
 
 const ProductModal = () => {
   const { product, handleChangeModal } = useMealsOrder();
+  const [quantity, setQuantity] = useState(1);
 
   return (
     <div className="md:flex gap-10">
@@ -39,6 +41,55 @@ const ProductModal = () => {
         <p className="mt-5 font-black text-5xl text-amber-500">
           {formatMoney(product.price)}
         </p>
+
+        <div className="flex gap-4 mt-5">
+          <button
+            type="button"
+            onClick={() => {
+              if (quantity <= 1) return;
+              setQuantity(quantity - 1);
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-7 w-7"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </button>
+
+          <p className="text-3xl">{quantity}</p>
+          <button
+            type="button"
+            onClick={() => {
+              if (quantity >= 5) return;
+              setQuantity(quantity + 1);
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-7 w-7"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </button>
+        </div>
       </div>
     </div>
   );
