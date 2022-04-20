c4b7c1e2c12746ae678011ad1bd4ecf77991d62c
inmacc96 <inmacaballerocarrero@gmail.com>
2022-04-20 19:27:35 +0200

    Navigate through the pages of the progress bar.

--- meals-order-app/components/ProgressBar.jsx
+++ meals-order-app/components/ProgressBar.jsx
@@ -1,3 +1,5 @@
+import { useRouter } from "next/router";
+
 const progressBar = [
   { progress: 1, name: "Menú", url: "/" },
   { progress: 2, name: "Resumen", url: "/summary" },
@@ -5,17 +7,20 @@ const progressBar = [
 ];
 
 const ProgressBar = () => {
+  const router = useRouter();
   return (
-      <>
+    <>
       <div className="flex justify-between mb-5">
-          {progressBar.map(progress => (
-              <button key={progress.progress} 
-              className="text-2xl font-bold">
-                  {progress.name}
-              </button>
-          ))}
+        {progressBar.map((progress) => (
+          <button key={progress.progress} className="text-2xl font-bold"
+          onClick={()=> {
+              router.push(progress.url) //Te redirigue a esa url
+          }}>
+            {progress.name}
+          </button>
+        ))}
       </div>
-      </>
+    </>
   );
 };
 
