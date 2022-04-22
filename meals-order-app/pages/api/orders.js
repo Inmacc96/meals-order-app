48ed5852439b3fe826ab8ae914646ce6f061da4e
inmacc96 <inmacaballerocarrero@gmail.com>
2022-04-22 14:42:31 +0200

    Inserting orders into the database with Prisma.

--- meals-order-app/pages/api/orders.js
+++ meals-order-app/pages/api/orders.js
@@ -1,7 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+
 export default async function handler(req, res) {
+  const prisma = new PrismaClient();
   if (req.method === "POST") {
-    console.log(req.body)
-  } else {
-    res.json({ metodo: "GET!!" });
+    const order = await prisma.order.create({
+      data: {
+        name: req.body.name,
+        date: req.body.date,
+        total: req.body.total,
+        orderInfo: req.body.orderInfo,
+      },
+    });
+    res.json(order);
   }
 }
