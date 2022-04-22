041e8da248bc6d25503bddb44e2fc83c9f8c0aa7
inmacc96 <inmacaballerocarrero@gmail.com>
2022-04-22 13:13:45 +0200

    Add order confirmation form on the total page.

--- meals-order-app/pages/total.js
+++ meals-order-app/pages/total.js
@@ -5,6 +5,36 @@ export default function Total() {
     <Layout page="Total y Confirmar Pedido">
       <h1 className="text-4xl font-black"> Total </h1>
       <p className="text-2xl my-10">Confirma tu Pedido a continuación</p>
+
+      <form>
+        <div>
+          <label
+            htmlFor="name"
+            className="block uppercase text-slate-800 font-bold text-xl"
+          >
+            Nombre
+          </label>
+
+          <input
+            id="name"
+            type="text"
+            className="bg-gray-200 w-full lg:w-1/3 rounded-md mt-3 p-2"
+          />
+        </div>
+
+        <div className="mt-10">
+          <p className="text-2xl">
+            Total a pagar: {""} <span className="font-bold">200€</span>
+          </p>
+        </div>
+
+        <div className="mt-5">
+          <input
+            className="bg-indigo-600 w-full lg:w-auto px-5 py-2 rounded uppercase font-bold text-white text-center hover:cursor-pointer"
+            value="Confirmar Pedido"
+          />
+        </div>
+      </form>
     </Layout>
   );
 }
